b9f3c5c45ce238a872037b07b4c0f359f13ce543
Justyna Konieczny <kojustynka@gmail.com>
2018-06-07 18:16:36 +0000

    integration - zajecia 7062018

--- web/app.py
+++ web/app.py
@@ -1,14 +1,31 @@
 
 from flask import Flask
 from flask import render_template
-from flask import request
+from flask import request, redirect, url_for
 from media.s3_storage import S3MediaStorage
 import os
 import boto3
+import json
 
+#class FilePhotoStack:
+    #def add_pending_photo(self, target_name) 
+#otworz plik, dopisz do pliku, zamknij plik
+
+
+
+class InMemoryPhotoStack:
+    def __init__(self):
+       self.photos_list = []
+    def get_all_photos(self):
+       return self.photos_list
+    def add_pending_photo(self, target_name):
+       self.photos_list.append(target_name) 
 s3 = boto3.resource('s3')
 storage = S3MediaStorage(s3, os.getenv('APP_BUCKET_NAME'))
 app = Flask(__name__)
+photo_stack = InMemoryPhotoStack()
+sqs = boto3.resource('sqs', region_name="eu-central-1")
+pending_orders = sqs.get_queue_by_name(QueueName='JUSTYNAsQUEUE')
 
 @app.route("/")
 def hello():
@@ -19,8 +36,21 @@ def make_upload():
     if 'filetoupload' not in request.files:
       return 'fail'
     myfile = request.files['filetoupload']
-    storage.store(dest="uploads/%s"%myfile.filename, source=myfile)
+    target_name = "uploads/%s"%myfile.filename
+    storage.store(dest=target_name, source=myfile)
+    photo_stack.add_pending_photo(target_name)
     return 'ok'
+
+@app.route("/order")
+def order():
+    return render_template('order.html', photos=photo_stack.get_all_photos())
+
+@app.route("/proceed", methods=['POST'])
+def proceed():
+    order_ani_request = {'email': request.form['email'], 'photos': photo_stack.get_all_photos()}
+    pending_orders.send_message(MessageBody=json.dumps(order_ani_request))
+
+    return redirect(url_for('order'))
 
 @app.route("/list")
 def list():
@@ -28,3 +58,4 @@ def list():
     return render_template('peoples.html', peoples=mydata)
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=8080, debug=True)
+
